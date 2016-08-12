83386dc8d34eb9345be6278bf13e042102c0aeb4
Camille Girabawe <cgira@brandeis.edu>
2016-08-12 01:19:41 -0400

    LyftObj documention

--- rideSharePy.py
+++ rideSharePy.py
@@ -8,7 +8,7 @@ import requests, json
 
 class UberObj():
     """
-    generate uber object around a location given its geo-coordinates
+    generate uber service object around a location given its geo-coordinates
     """
     def __init__(self):
         # load credentials
@@ -19,7 +19,7 @@ class UberObj():
     
     def get_products(self,lat,lon):
         """
-        get objects around a given location
+        get uber products around a given location
         """
         products = self.uber.get_products(lat,lon)['products']
         return products
@@ -37,43 +37,81 @@ class UberObj():
         return prices
 
 class LyftObj():
+    """
+    generate lyft service object
+    """
     def __init__(self):
+        """
+        initialize connection to the api url and get access token
+        """
+        # load credentials from file
         credentials = Lyftapp_credentials()
         self.client_id = credentials.client_id
         self.client_secret = credentials.client_secret
+        
+        # obtain access token
         self.token = self.__generate_token__()
         
+        # define variables to be used in the request parameters
         token_val = 'Bearer '+self.token
         self.headers = {'Authorization':token_val}
         
     def __generate_token__(self):
+        """
+        use client_id and client_secret to generate access token. 
+        The generated access code will only last for 24 hours
+        """
         url = 'https://api.lyft.com/oauth/token'
+        
+        # define request parameters
         payload = {"Content-Type": "application/json",
                    "grant_type": "client_credentials", 
                    "scope": "public"}
+        # request data
         res = requests.post(url, 
                             data = payload,
                             auth = (self.client_id, self.client_secret))
-        json_output = res.json()
-        token = json_output['access_token']
+        
+        # extract the token from the response
+        token = res.json()['access_token']
         return token
 
     def get_nearby_drivers(self,lat,lon):
+        """
+        get drivers nearby a given location
+        lat: float, latitude
+        lon: float, longitude
+        """
         url = 'https://api.lyft.com/v1/drivers?lat='+str(lat)+'&lng='+str(lon)
         nearby_drivers = requests.get(url,headers=self.headers).json()['nearby_drivers']
         return nearby_drivers
     
     def get_ride_types(self,lat,lon):
+        """
+        get lyft products around a given location
+        lat: float, latitude
+        lon: float, longitude
+        """
         url = 'https://api.lyft.com/v1/ridetypes?lat='+str(lat)+'&lng='+str(lon)
         ride_types = requests.get(url,headers=self.headers).json()['ride_types']
         return ride_types
     
     def get_ride_cost(self,st_lat,st_lon,en_lat,en_lon):
+        """
+        get cost estimates of a ride given starting and ending coordinates
+        lat: float, latitude
+        lon: float, longitude
+        """
         url = 'https://api.lyft.com/v1/cost?start_lat='+str(st_lat)+'&start_lng='+str(st_lon)+'&end_lat='+str(en_lat)+'&end_lng='+str(en_lon)
         ride_cost = requests.get(url,headers=self.headers).json()['cost_estimates']
         return ride_cost
     
     def get_eta(self,lat,lon):
+        """
+        get the estimate time of arrival with respect to a given location
+        lat: float, latitude
+        lon: float, longitude
+        """
         url = 'https://api.lyft.com/v1/eta?lat='+str(lat)+'&lng='+str(lon)
         eta = requests.get(url,headers=self.headers).json()['eta_estimates']
         return eta
