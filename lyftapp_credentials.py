5c52872ec7ae01fc8900723dddedcf31a240d1be
kthouz <cgira@brandeis.edu>
2016-08-12 00:57:17 -0400

    update

--- lyftapp_credentials.py
+++ lyftapp_credentials.py
@@ -5,7 +5,6 @@ class Lyftapp_credentials():
         """
         This generate an object containing uber application credentials
         """
-        self.app_dashboard = 'https://www.lyft.com/developers/manage/WOBWVGD2RS4h'
         credentials = pd.read_csv('Lyft/credentials.csv')
         self.client_id = credentials['CLIENT_ID'].values[0]
         self.server_token = credentials['SERVER_TOKEN'].values[0]
--- uberapp_credentials.py
+++ uberapp_credentials.py
@@ -5,7 +5,6 @@ class Uberapp_credentials():
         """
         This generate an object containing uber application credentials
         """
-        self.app_dashboard = 'https://developer.uber.com/dashboard/app/r7C-MyNyQr6jy3VxrRQAPVbCBkKJY56k'
         credentials = pd.read_csv('uber/credentials.csv')
         self.cliend_id = credentials['CLIENT_ID'].values[0]
         self.server_token = credentials['SERVER_TOKEN'].values[0]
